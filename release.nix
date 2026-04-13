2567884ac23c96c5a5d6afbbe2a22114f683f858
ci <ci@users.noreply.github.com>
2026-04-13 15:31:32 +0000

    release: sticky-20260413152341-792f4e25

--- release.nix
+++ release.nix
@@ -1,6 +1,6 @@
 {
   url = "https://github.com/LoneExile/AeroSpace/releases/download/sticky-rolling/AeroSpace.zip";
-  sha256 = "sha256-mK8ljAsScfTa0L/yuTsJpeysreRbnatBntWePqDAO5c=";
-  commit = "2e972f3a89334be0e7b5e28c3dfff032ee8a91df";
-  version = "sticky-20260413142204-2e972f3a";
+  sha256 = "sha256-gwFWXyGr48UyM/GfZt4ugYGy5CDQzJ0A0burynwZDP8=";
+  commit = "792f4e255877ba221025f43ec3c1af173a356790";
+  version = "sticky-20260413152341-792f4e25";
 }
