3eb8b93f19fd15783873fc8ab6f11d4b440672a9
LoneExile <82561297+LoneExile@users.noreply.github.com>
2026-04-13 22:51:09 +0700

    hotfix: use fetchzip-compatible NAR hash (unpacked dir, not zip bytes)

--- release.nix
+++ release.nix
@@ -1,6 +1,6 @@
 {
   url = "https://github.com/LoneExile/AeroSpace/releases/download/sticky-rolling/AeroSpace.zip";
-  sha256 = "sha256-gwFWXyGr48UyM/GfZt4ugYGy5CDQzJ0A0burynwZDP8=";
+  sha256 = "sha256-Ecg77SnN/D4hdnRBzhJBvLjTysmELEp4CljGsinvmyY=";
   commit = "792f4e255877ba221025f43ec3c1af173a356790";
   version = "sticky-20260413152341-792f4e25";
 }
