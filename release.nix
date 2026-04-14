7503c218fb9d6182dd599c7bcc352db2104cf42a
LoneExile <82561297+LoneExile@users.noreply.github.com>
2026-04-14 08:53:27 +0700

    hotfix: match actual NAR hash observed locally

--- release.nix
+++ release.nix
@@ -1,6 +1,6 @@
 {
   url = "https://github.com/LoneExile/AeroSpace/releases/download/sticky-rolling/AeroSpace.zip";
-  sha256 = "sha256-npPSsrVYVROrqD9EsS/YYnKATmX5BTmJm/acd6eZOqs=";
+  sha256 = "sha256-Z1Anr9u5vkfmV3gthgz9HE2jKeMtTJsJ6f1jJ6l1ArY=";
   commit = "b21622b8ee8fc22d1a8d82b14d680606a9efbf4c";
   version = "sticky-20260414013228-b21622b8";
 }
